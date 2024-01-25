2a62a63c748547c3ce61b2221f038ebc974ccb4f
Marcelo Emmerich <m.emmerich@conventic.com>
2024-01-25 13:07:09 +0100

    recurse and out sol files in array

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import typer
 import os
+import glob
 from typing_extensions import Annotated
 
 
 def main(root_dir: Annotated[str, typer.Argument()] = os.getcwd()):
     print(f"Recursing from: {root_dir}")
+    # list to store txt files
+    res = []
+    # os.walk() returns subdirectories, file from current directory and 
+    # And follow next directory from subdirectory list recursively until last directory
+    for root, dirs, files in os.walk(root_dir):
+        for file in files:
+            if file.endswith(".sol"):
+                res.append(os.path.join(root, file))
+    print(res)
 
 
 if __name__ == "__main__":
